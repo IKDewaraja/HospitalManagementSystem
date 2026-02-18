15a82465ad5db16f5c944374406d56f42d4c7805
ikdewaraja <imandidewaraja281@gmail.com>
2026-02-18 17:04:28 +0530

    admin-serive complete

---
+++ admin-service/src/main/java/com/hospital/admin_service/Doctor.java
@@ -0,0 +1,24 @@
+package com.hospital.admin_service;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "doctors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String specialization;
+
+    private String consultationFee;
+}
---
+++ admin-service/src/main/java/com/hospital/admin_service/DoctorController.java
@@ -0,0 +1,23 @@
+package com.hospital.admin_service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    @PostMapping
+    public Doctor createDoctor(@RequestBody Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+}
